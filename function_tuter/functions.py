3b1f75ef6e23856308aece06255a956dbe10554c
Ignl94 <65883584+Ignl94@users.noreply.github.com>
2020-09-06 17:26:58 -0500

    Add files via upload

---
+++ function_tuter/functions.py
@@ -0,0 +1,33 @@
+def area(width, height):
+    result = width * height
+    return result
+
+
+result = area(5, 6)
+print(result)
+result = area(5, 8)
+print(result)
+result = area(9, 12)
+print(result)
+
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+
+result = subtract(10, 2)
+print(result)
+result = subtract(34, 12)
+print(result)
+
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+
+result = add(5, 6)
+print(result)
+result = add(7, 8)
+print(result)
